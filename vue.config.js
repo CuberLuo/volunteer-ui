1b7ea35b01d20c38d43b00e3a60aa984cd8fda39
CuberLuo <1501858187@qq.com>
2022-05-26 09:24:23 +0800

    注释修改

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://182.61.20.232:8085', // Wampserver端口号为80
+        target: 'http://182.61.20.232:8085', // 云服务器tomcat端口号8085
         pathRewrite: { '^/api': '' },
         // 例如将 http://localhost:8080/api/login 变为 http://localhost:80/login
         changeOrigin: true // true表示代理服务器告诉80服务器自己来自http://localhost:80
